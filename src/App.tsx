dde6f5f04bd743c50decc6abf263a6d86bf3e568
anix001 <chaudharyanish305@gmail.com>
2024-10-19 16:31:09 +0545

    Improvement: React Navigation project

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+} from 'react-native';
+
+//navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//screens
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {product: Product}
+};
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
+function App(){
+  return(
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} options={{title:"Trending Projects"}}/>
+      <Stack.Screen name="Details" component={Details} options={{title:"Product Details"}}/>
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
+
+
+export default App;
